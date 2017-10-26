aeabcf9f05b73f55e7b7f903654f6b66b961d512
itaouil <itaouil95@gmail.com>
2017-10-26 16:32:24 +0100

    Added points rotation for C1 and C2.

---
+++ config.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env/python
+
+"""
+    The following Python script
+    contains values used in the
+    *_code.py script, such as the
+    angle chosen for the R matrix,
+    as well as the rectangles size,
+    and more.
+
+    Author      : Ilyass Taouil
+    Module      : Machine Learning
+    Instructor  : Dr. Matteo Leonetti
+"""
+
+data = {
+
+    # Angle for R matrix
+    "angle"     : 75,
+
+    # Number of points
+    "size"      : 500,
+
+    # C1 rectagle size
+    "c1_x_low"  : 2,
+    "c1_x_high" : 5,
+    "c1_y_low"  : 1,
+    "c1_y_high" : 4,
+
+    # C2 rectagle size
+    "c2_x_low"  : 1,
+    "c2_x_high" : 3,
+    "c2_y_low"  : -5,
+    "c2_y_high" : -1,
+
+}
--- taouil_ilyass_code.py
+++ taouil_ilyass_code.py
@@ -12,22 +12,40 @@
 """
 
 # Import packages
+import math
 import numpy as np
 import matplotlib.pyplot as plt
+
+# Import configuration file
+import config as cf
 
 # Generate datasets
 def generate_dataset():
 
     # Class1 points: 2D array formed by stacking x and y arrays
-    c1 = np.column_stack((np.random.uniform(2, 5, 500), np.random.uniform(1, 4, 500)))
+    c1 = np.column_stack((np.random.uniform(cf.data["c1_x_low"], cf.data["c1_x_high"], cf.data["size"]),
+                          np.random.uniform(cf.data["c1_y_low"], cf.data["c1_y_high"], cf.data["size"])))
+    print("C1",c1)
 
     # Class2 points: 2D array formed by stacking x and y arrays
-    c2 = np.column_stack((np.random.uniform(1, 3, 500), np.random.uniform(-5, -1, 500)))
+    c2 = np.column_stack((np.random.uniform(cf.data["c2_x_low"], cf.data["c2_x_high"], cf.data["size"]),
+                          np.random.uniform(cf.data["c2_y_low"], cf.data["c2_y_high"], cf.data["size"])))
+    print("C2",c2)
+
+    # Define matrix R
+    R = np.array([ [math.cos(cf.data["angle"]), -(math.sin(cf.data["angle"]))],
+                   [math.sin(cf.data["angle"]), math.cos(cf.data["angle"])] ])
+    print("R",R)
+
+    # C1 and C2 points rotation
+    for x in range(len(c1)):
+        c1[x] = np.dot(c1[x], R)
+        c2[x] = np.dot(c2[x], R)
 
     # Plot C1 and C2
     plt.plot(c1.T[0], c1.T[1], 'ro')
     plt.plot(c2.T[0], c2.T[1], 'bo')
-    plt.axis([0,10,-8,10])
+    plt.axis([0,14,-6,6])
     plt.show()
 
 generate_dataset()
