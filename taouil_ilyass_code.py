9367a22bc952354f42dd6b5ce42c30d26476738b
itaouil <itaouil95@gmail.com>
2017-11-01 15:18:00 +0000

    Cleaned code.

--- taouil_ilyass_code.py
+++ taouil_ilyass_code.py
@@ -44,6 +44,48 @@ def encode(dataset):
 
     return targets
 
+# Decode class
+def decode(ouput):
+
+    if output == [1,0,0,0]:
+        return 1
+    elif output == [0,1,0,0]:
+        return 2
+    elif output == [0,0,1,0]:
+        return 3
+    elif output == [0,0,0,1]:
+        return 4
+    else:
+        return -1
+
+# Vectorized sigmoid function
+def vsigmoid(x):
+
+    # Sigmoid function vectorized
+    f = np.vectorize(lambda x: 1 / (1 + math.exp(-1 * x)))
+
+    return f(x)
+
+# Error function
+def errors(targets, outputs):
+
+    # Sum of Squared Difference function
+    f = np.vectorize(lambda x,y: 0.5 * (x - y) ** 2)
+
+    return f(targets, outputs)
+
+# Stacks two arrays together
+def stack(A, B):
+    return np.column_stack((A, B))
+
+# Generates uniform data
+def uniform(l, h, size):
+    return np.random.uniform(l, h, size)
+
+# Generates multivariate data
+def multivariate(mean, cov, size):
+    return np.random.multivariate_normal(mean, cov, size)
+
 """
     Generates datasets and
     returns the three sets
@@ -54,40 +96,39 @@ def encode(dataset):
 """
 def generate_dataset():
 
-    # Generate points for class1
-    c1 = np.column_stack((np.random.uniform(cf.data["c1_x_low"], cf.data["c1_x_high"], cf.data["size"]),
-                          np.random.uniform(cf.data["c1_y_low"], cf.data["c1_y_high"], cf.data["size"])))
-
-    # Generate points for class2
-    c2 = np.column_stack((np.random.uniform(cf.data["c2_x_low"], cf.data["c2_x_high"], cf.data["size"]),
-                          np.random.uniform(cf.data["c2_y_low"], cf.data["c2_y_high"], cf.data["size"])))
-
     # Rotation matrix (R)
     R = np.array([ [math.cos(cf.data["angle"]), -math.sin(cf.data["angle"])],
                    [math.sin(cf.data["angle"]), math.cos(cf.data["angle"])] ])
 
+    # Generate points for class1
+    c1 = stack(uniform(cf.data["c1_x_low"], cf.data["c1_x_high"], cf.data["size"]),
+               uniform(cf.data["c1_y_low"], cf.data["c1_y_high"], cf.data["size"]))
+
+    # Generate points for class2
+    c2 = stack(np.random.uniform(cf.data["c2_x_low"], cf.data["c2_x_high"], cf.data["size"]),
+               np.random.uniform(cf.data["c2_y_low"], cf.data["c2_y_high"], cf.data["size"]))
+
     # Rotate class1 and class2 points
-    for x in range(cf.data["size"]):
-        c1[x] = np.dot(c1[x], R)
-        c2[x] = np.dot(c2[x], R)
+    c1 = np.dot(c1, R)
+    c2 = np.dot(c2, R)
 
     # Generate points for class3
-    c3 = np.random.multivariate_normal(cf.data["c3_mean"], cf.data["c3_cov"], cf.data["size"])
+    c3 = multivariate(cf.data["c3_mean"], cf.data["c3_cov"], cf.data["size"])
 
     # Generate points for class4
-    c4 = np.random.multivariate_normal(cf.data["c4_mean"], cf.data["c4_cov"], cf.data["size"])
+    c4 = multivariate(cf.data["c4_mean"], cf.data["c4_cov"], cf.data["size"])
 
     # Add columns of 1s to class1
-    class1 = np.column_stack((c1, np.full(cf.data["dim"], 1)))
+    class1 = stack(c1, np.full(cf.data["dim"], 1))
 
     # Add columns of 2s to class2
-    class2 = np.column_stack((c2, np.full(cf.data["dim"], 2)))
+    class2 = stack(c2, np.full(cf.data["dim"], 2))
 
     # Add columns of 3s to class3
-    class3 = np.column_stack((c3, np.full(cf.data["dim"], 3)))
+    class3 = stack(c3, np.full(cf.data["dim"], 3))
 
     # Add columns of 4s to class4
-    class4 = np.column_stack((c4, np.full(cf.data["dim"], 4)))
+    class4 = stack(c4, np.full(cf.data["dim"], 4))
 
     # Aggregate classes together in one bigger matrix (2000,3)
     matrix = np.concatenate((class1, class2, class3, class4))
@@ -99,18 +140,53 @@ def generate_dataset():
     matrix[:,:2] = (matrix[:,:2] - matrix[:,:2].mean(axis=0)) / matrix[:,:2].var(axis=0)
 
     # Plot classes' points
-    # plt.plot(c1.T[0], c1.T[1], 'ro')
-    # plt.plot(c2.T[0], c2.T[1], 'bo')
-    # plt.plot(c3.T[0], c3.T[1], 'go')
-    # plt.plot(c4.T[0], c4.T[1], 'co')
-    # plt.axis([-10,7,-10,6])
-    # plt.show()
+    plt.plot(c1.T[0], c1.T[1], 'ro')
+    plt.plot(c2.T[0], c2.T[1], 'bo')
+    plt.plot(c3.T[0], c3.T[1], 'go')
+    plt.plot(c4.T[0], c4.T[1], 'co')
+    plt.axis([-10,7,-10,6])
+    plt.show()
 
     # Returns trzining, evaluation and testing sets
     return matrix[:1000], matrix[1000:1500], matrix[1500:]
 
 """
-    Trzin the MLP with the
+    Classifies a point
+    using the trained
+    MLP.
+"""
+def classify_mlp(w1,w2,x):
+
+    # Forward phase (hidden layer) + sigmoid + bias input
+    zj = np.column_stack((vsigmoid(np.dot(x, w1)), np.full((np.shape(x)[0], 1), -1).ravel()))
+
+    # Forward phase (output layer) + sigmoid
+    zk = vsigmoid(np.dot(zj, w2))
+
+    # Decode output
+    return decode(zk)
+
+"""
+    Evalutates error
+    on the validation
+    set.
+"""
+def evaluate_mlp(w1,w2,D):
+
+    # Get targets of dataset
+    targets = encode(D)
+
+    # Forward phase (hidden layer) + sigmoid + bias input
+    zj = np.column_stack((vsigmoid(np.dot(x, w1)), np.full((np.shape(x)[0], 1), -1).ravel()))
+
+    # Forward phase (output layer) + sigmoid
+    zk = vsigmoid(np.dot(zj, w2))
+
+    # Compute errors
+    errors = np.sum(errors(targets, zk))
+
+"""
+    Trains the MLP with the
     forward fit, followed
     by the backpropagation
     phase.
@@ -123,16 +199,21 @@ def train_mlp(w1,w2,h,eta,D):
     # Get targets for training dataset
     t = encode(D)
 
-    # Vectorized sigmoid function
-    vsigmoid = np.vectorize(lambda x: 1 / (1 + math.exp(-1 * x)))
-
     # Add bias input to D (in place of target)
     D[:, 2] = np.full((np.shape(D)[0], 1), -1).ravel()
 
-    # Forward phase (hidden layer) + sigmoid + bias input
+    # Forward phase (hidden layer)
+    # Please note that the sigmoid
+    # is applied directly to the dot
+    # product and the result of it
+    # is stacked together with a
+    # bias input
     zj = np.column_stack((vsigmoid(np.dot(D, w1)), np.full((np.shape(D)[0], 1), -1).ravel()))
 
-    # Forward phase (output layer) + sigmoid
+    # Forward phase (output layer)
+    # Please note that the sigmoid
+    # function is applied directly
+    # to the dot product
     zk = vsigmoid(np.dot(zj, w2))
 
     for r in range(len(D)):
@@ -171,7 +252,7 @@ def main():
     # Get datasets
     training, validation, test = generate_dataset()
 
-    # Call MLP trzining
+    # Call MLP training
     train_mlp(w1, w2, 3, 0.3, training)
 
 # Check if the node is executing in the mzin path
